fbe9ea7ea90242b264c672b46bd495a9e464712e
Antonio Cano <jacj160901a@gmail.com>
2022-04-25 00:15:59 -0500

    Eliminar-Editar

--- src/app/alumnos/alumnos.component.ts
+++ src/app/alumnos/alumnos.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit , Input} from '@angular/core';
+import { Component, OnInit , Input, OnChanges, SimpleChanges} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { DatabaseService } from '../database.service';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-alumnos',
   templateUrl: './alumnos.component.html',
   styleUrls: ['./alumnos.component.css']
 })
-export class AlumnosComponent implements OnInit {
+export class AlumnosComponent implements OnInit, OnChanges {
 
   constructor(private db: DatabaseService) { }
 
+  ngOnChanges(changes: SimpleChanges): void {
+      
+      this.consultaAlumnos();
+      window.location.reload();
+  }
+
  alumnos: any=[];
+ buffer: any=[];
+
   ngOnInit(): void {
    this.consultaAlumnos();
   }
@@ -30,10 +39,35 @@ export class AlumnosComponent implements OnInit {
       this.alumnos.push(nuevoAlumno)
     }
 
+    status: boolean=false;
+
     consultaAlumnos(){
       this.db.getAlumnos().subscribe(res=>{
         this.alumnos=res;
       });
+      
     }
-    
+
+    eliminar(id:number, alumnoBorrar: any){
+      this.status=false;
+      /*this.db.borrarAlumno(id).subscribe(res=>{
+        console.log('Finished')
+        this.status=true;
+      });
+      */
+      for(var x=id; x<this.alumnos.length; x++){
+        console.log('x: '+ x+' '+this.alumnos[x]);
+        this.db.actualizarAlumno(x,this.alumnos[1+x]).subscribe();
+        console.log('x+1: '+x+' '+this.alumnos[x+1]);
+        if((x+1)==this.alumnos.length){
+          this.db.borrarAlumno(this.alumnos.length-1).subscribe();
+          console.log(this.alumnos.length-1);
+          this.status=true
+        }
+        if(this.status){
+          this.consultaAlumnos();
+        }
+      }
+    }
+
 }
--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -18,8 +18,15 @@ export class DatabaseService {
   //post
 
   //update
+  actualizarAlumno(id: number, alumno: any){
+    return this.http.put('https://alumnos-59d84-default-rtdb.firebaseio.com/alumnos/'+ id +'.json', alumno)
+  }
 
-
+  arreglarAlumnos(alumno:any, id:number){
+    return this.http.put('https://alumnos-59d84-default-rtdb.firebaseio.com/alumnos.json'+ id +'.json', alumno)
+  }
   //delete
-
+  borrarAlumno(id: number){
+    return this.http.delete('https://alumnos-59d84-default-rtdb.firebaseio.com/alumnos/'+ id +'.json')
+  }
 }
