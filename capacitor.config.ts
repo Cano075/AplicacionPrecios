88cb8613ca85fc605ae5aa1ba92a49e00a69a03b
Antonio Cano <jacj160901a@gmail.com>
2022-05-17 01:33:39 -0500

    apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'angular1914173',
+  appName: 'app',
   webDir: 'www',
   bundledWebRuntime: false
 };
